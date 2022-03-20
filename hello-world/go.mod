ea5b46e7704f1129863be39ed34616257418d82e
Paul Boersma <pcboersma91@gmail.com>
2022-03-20 20:09:48 +0100

    Use gorilla/mux third party package as router for handling complex routing requests

--- hello-world/go.mod
+++ hello-world/go.mod
@@ -1,3 +1,5 @@
 module github.com/Paul-Boersma/go-tutorials/hello-world
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
--- hello-world/main.go
+++ hello-world/main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	// use third party package for handling complex routing requests
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	// handle a request for localhost:8080
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello World")
+	// Define router to handle complex routing requests
+	r := mux.NewRouter()
+
+	// Process dynamic requests: Process incoming requests from users who browse the website, log into their account
+	// Request contains all information needed for processing requests (r.URL.Query().Get("token"), (r.FormValue("email")))
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello World!")
+	})
+	// Use gorilla/mux instead of regular http package to handle complex routing requests
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+		fmt.Println("title: ", title, "page: ", page)
+		// get the book
+		// navigate to page
 	})
 
-	// Start the server on port 8080
-	http.ListenAndServe(":8080", nil)
+	// Serve static assets: Serve Javascript, CSS and images to browsers
+	// 1. Build a fileserver
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Accept connections: Start the server on port 8080
+	http.ListenAndServe(":8090", r)
 }
